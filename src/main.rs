9cf91439d1b33b294860bb07a60d986f6d82e3fc
ZephOne <erwan.colin@mines-albi.fr>
2018-06-17 20:56:51 +0200

    Use reqwest instead of curl

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,28 @@
-extern crate a_to_b;
-extern crate curl;
+extern crate reqwest;
 
-use a_to_b::Trajet;
-use curl::easy::Easy;
-use std::io::{ stdout, Write };
+fn get_json(url: &str) -> String {
+    let res = reqwest::get(url);
+    let mut res = match res {
+        Ok(response) => response,
+        Err(e) => {
+            panic!("There was an error making an HTTP GET: {}", e)
+        }
+    };
+
+    let body = res.text();
+    let body = match body {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("There was a problem getting the HTTP GET content")
+        },
+    };
+
+    body
+}
 
 fn main() {
-    println!("Welcome to A to B, the distance giver program!\n");
 
-    let trajet = Trajet::new();
+    let city1_json = get_json("https://nominatim.openstreetmap.org/search?city=proussy&format=json");
 
-    let mut start_city = trajet.start_city.to_lowercase();
-    let mut end_city = trajet.end_city.to_lowercase();
-
-    // let mut city_url = format!("https://nominatim.openstreetmap.org/search?q={}&format=json", &start_city);
-    
-    let start_city_url = "https://nominatim.openstreetmap.org/search?q=proussy&format=json";
-    let mut easy_start_city = Easy::new();
-    let user_agent = "AToB";
-    easy_start_city.useragent(&user_agent);
-    easy_start_city.url(&start_city_url).unwrap();
-    easy_start_city.write_function(|data| {
-        stdout().write_all(data).unwrap();
-        Ok(data.len())
-    }).unwrap();
-    easy_start_city.perform().unwrap();
-    let json_start_city = easy_start_city.response_code().unwrap();
-
-    println!("{}", json_start_city);
-
-    let end_city_url = "https://nominatim.openstreetmap.org/search?q=toulouse&format=json";
-    let mut easy_end_city = Easy::new();
-    let user_agent = "AToB";
-    easy_end_city.useragent(&user_agent);
-    easy_end_city.url(&end_city_url).unwrap();
-    easy_end_city.write_function(|data| {
-        stdout().write_all(data).unwrap();
-        Ok(data.len())
-    }).unwrap();
-    easy_end_city.perform().unwrap();
-    let json_end_city = easy_end_city.response_code().unwrap();
-
-    println!("{}", json_end_city);
+    println!("{}", city1_json);
 }
