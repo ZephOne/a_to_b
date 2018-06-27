e88ccb310786432e0c5fc1baf26bee79e1d28fe9
ZephOne <erwan.colin@mines-albi.fr>
2018-06-28 01:36:56 +0200

    Implement Coordinate getter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+extern crate reqwest;
+extern crate serde_json;
+
 use std::io;
+use serde_json::{Value, Error};
 
 pub struct Trajet {
     pub start_city: String,
@@ -19,4 +23,37 @@ impl Trajet {
 
         Trajet{start_city: start_city, end_city: end_city, duration: 0, distance: 0}
     }
+}
+
+pub struct Coordinates {
+    pub lat: String,
+    pub lon: String,
+}
+
+pub fn get_body(url: &str) -> String {
+    let res = reqwest::get(url);
+    let mut res = match res {
+        Ok(response) => response,
+        Err(e) => {
+            panic!("There was an error making an HTTP GET: {}", e)
+        }
+    };
+
+    let body = res.text();
+    let body = match body {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("There was a problem getting the HTTP GET content")
+        },
+    };
+
+    body
+}
+
+pub fn get_coordinates(json: &str) -> Coordinates {
+    let v: Value = serde_json::from_str(&json[..]).unwrap();
+    let u = &v[0];
+    let lat = &u["lat"];
+    let lon = &u["lon"];
+    Coordinates {lat: lat.to_string(), lon: lon.to_string(),}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-extern crate reqwest;
+extern crate a_to_b;
 
-fn get_json(url: &str) -> String {
-    let res = reqwest::get(url);
-    let mut res = match res {
-        Ok(response) => response,
-        Err(e) => {
-            panic!("There was an error making an HTTP GET: {}", e)
-        }
-    };
-
-    let body = res.text();
-    let body = match body {
-        Ok(content) => content,
-        Err(error) => {
-            panic!("There was a problem getting the HTTP GET content")
-        },
-    };
-
-    body
-}
+use a_to_b::{get_body, get_coordinates};
 
 fn main() {
 
-    let city1_json = get_json("https://nominatim.openstreetmap.org/search?city=proussy&format=json");
+    let city1_response_body = a_to_b::get_body("https://nominatim.openstreetmap.org/search?city=proussy&format=json");
+    println!("{}", city1_response_body);
 
-    println!("{}", city1_json);
+    let city1_coordinates = a_to_b::get_coordinates(&city1_response_body[..]);
+
+    let lat = &city1_coordinates.lat;
+    let lon = &city1_coordinates.lon;
+
+    println!("La latitude de proussy est {}", lat);
+    println!("La longitude de proussy est {}", lon);
 }
